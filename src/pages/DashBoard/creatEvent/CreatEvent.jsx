c94e84402400ea3f0b42ebf61e830b85c55cd848
Mohammad Rafe <mrrafi5869@gmail.com>
2024-01-02 21:41:22 +0600

    schedule events

--- src/pages/DashBoard/creatEvent/CreatEvent.jsx
+++ src/pages/DashBoard/creatEvent/CreatEvent.jsx
@@ -1,6 +1,6 @@
 const CreateEvent = () => {
   return (
-    <div className="flex items-center justify-center ">
+    <div className="">
       <h1 className="text-7xl text-red-500">this is create event</h1>
     </div>
   );
