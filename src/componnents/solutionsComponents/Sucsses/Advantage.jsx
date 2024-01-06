0bdb313f30928f694ac9771d91b8ec09c8bb921a
Mohammad Rafe <mrrafi5869@gmail.com>
2024-01-06 14:09:03 +0600

    solutions

---
+++ src/componnents/solutionsComponents/Sucsses/Advantage.jsx
@@ -0,0 +1,17 @@
+import EasyScheduleSecondPart from "../../../pages/solutions/solutions/EasyScheduleSecondPart";
+
+const Advantage = () => {
+  return (
+    <div>
+      <EasyScheduleSecondPart
+        title="GAIN A COMPETITIVE ADVANTAGE"
+        heading="Connect customers with the right expert"
+        para="Improve your response and resolution times. Route customers to a specific specialist or customer advocate for answers or round robin meetings among CS reps to solve needs even faster."
+        link="Routes Customer To Support"
+        img="https://images.ctfassets.net/k0lk9kiuza3o/4V0yARXJiPrV8qZQYQEPc9/fa9e13babaa234f30fcedd03eb935341/Calendly-Support-Times.png?q=85&fm=webp"
+      ></EasyScheduleSecondPart>
+    </div>
+  );
+};
+
+export default Advantage;
