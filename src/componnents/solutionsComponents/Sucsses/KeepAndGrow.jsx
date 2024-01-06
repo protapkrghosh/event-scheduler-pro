6adeeae7c6bbf4cc1f6564f8012f30c1288a492a
Mohammad Rafe <mrrafi5869@gmail.com>
2024-01-06 13:49:41 +0600

    solutions

---
+++ src/componnents/solutionsComponents/Sucsses/KeepAndGrow.jsx
@@ -0,0 +1,17 @@
+import EasyScheduleFirstPart from "../../../pages/solutions/solutions/EasyScheduleFirstPart";
+
+const KeepAndGrow = () => {
+  return (
+    <div>
+      <EasyScheduleFirstPart
+        title="FILL YOUR PIPELINE FASTER"
+        heading="Get instant speed to lead"
+        para="Connect with your top leads before a competitor does. Don’t miss out on opportunities — instantly book qualified meetings from your website and match inbound leads to the right sales"
+        link="Screen and book from your website"
+        img="https://images.ctfassets.net/k0lk9kiuza3o/47ELRKmZZdpjpLHOAxHEwK/98bd01d677e748cd255f0d252c78f5d0/outbound.png?q=85&fm=webp"
+      ></EasyScheduleFirstPart>
+    </div>
+  );
+};
+
+export default KeepAndGrow;
--- src/componnents/solutionsComponents/marketing/Marketing.jsx
+++ src/componnents/solutionsComponents/marketing/Marketing.jsx
@@ -1,9 +1,13 @@
+import ImproveConversationRate from "./ImproveConversationRate";
+import IncreaseSpeedToLeed from "./IncreaseSpeedToLeed";
 import MarketingHero from "./MarketingHero";
 
 const Marketing = () => {
   return (
     <div>
       <MarketingHero />
+      <ImproveConversationRate></ImproveConversationRate>
+      <IncreaseSpeedToLeed></IncreaseSpeedToLeed>
     </div>
   );
 };
