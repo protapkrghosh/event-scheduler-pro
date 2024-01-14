a1eee8e260418792935e944575f3bca8005fa548
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-01-14 18:24:34 +0600

    server added

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -5,7 +5,7 @@ const useAdmin = (email) => {
   const [isAdminLoading, setIsAdminLoading] = useState(true);
   useEffect(() => {
     if (email) {
-      fetch(`http://localhost:3000/users/admin/${email}`)
+      fetch(`https://lets-sheduleit-backend.vercel.app/users/admin/${email}`)
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
--- src/hooks/useSingleEvents.jsx
+++ src/hooks/useSingleEvents.jsx
@@ -6,7 +6,7 @@ const useSingleEvents = (id) => {
     queryKey: ["events", id],
     queryFn: async () => {
       const res = await axios.get(
-        `http://localhost:3000/api/v1/events/get-SingleEvents?id=${id}`
+        `https://lets-sheduleit-backend.vercel.app/api/v1/events/get-SingleEvents?id=${id}`
       );
       return res.data;
     },
