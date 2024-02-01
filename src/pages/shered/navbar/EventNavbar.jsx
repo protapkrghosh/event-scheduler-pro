1cd5ec6e6ebc85ca379da99c1e04f5b38e245217
Mohammad Rafe <mrrafi5869@gmail.com>
2024-02-01 16:39:19 +0600

    sdfs

--- src/pages/shered/navbar/EventNavbar.jsx
+++ src/pages/shered/navbar/EventNavbar.jsx
@@ -8,22 +8,6 @@ const EventNavbar = () => {
         <FaLink /> copy link
       </button>
 
-      <div className="dropdown dropdown-bottom dropdown-end">
-        <div tabIndex={0} role="button" className="">
-          Menu
-        </div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-        </ul>
-      </div>
       <DashBoardNavbarComponnets />
     </div>
   );
